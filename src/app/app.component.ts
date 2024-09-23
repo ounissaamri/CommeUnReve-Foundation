99a3be6939a5a0875e889e089cdab9882839395a
Amri Ounissa <amriounissa@MacBook-Pro-de-Amri.local>
2024-09-23 23:43:38 +0200

    update header component avec html et scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,10 @@ import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+     HeaderComponent,
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    RouterOutlet,
+    MatCardModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
