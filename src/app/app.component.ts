4ef27f3dddfc28eabc2ad79137d856d2622ed74c
Burdy <burdy.goureige.ep@whoz.com>
2024-10-08 18:34:37 +0200

    ajout police

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { HeaderComponent } from './shared/components/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-     HeaderComponent,
+    HeaderComponent,
+    HomeComponent
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
