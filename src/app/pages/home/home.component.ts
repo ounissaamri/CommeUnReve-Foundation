f9b2aa419a54c9b199d6d90078df63ee15794b1b
Ounissa Amri <78350734+ounissaamri@users.noreply.github.com>
2024-10-14 19:43:38 +0200

    creation de la page nos actions et devenir benevole

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BlogComponent } from '../../shared/components/blog/blog.component';
 import { DonationComponent } from '../../shared/components/donation/donation.component';
 import { HerosectionComponent } from '../../shared/components/herosection/herosection.component';
@@ -19,5 +19,9 @@ import { MissionsComponent } from '../../shared/components/missions/missions.com
 })
 export class HomeComponent {
   
-
+  @Input() titre! : string
+  @Input() text! : string
+  @Input() button!: string 
+  @Input() titreb!:string 
+  @Input() descriptionb!: string
 }
