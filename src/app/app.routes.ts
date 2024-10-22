dddf9d3ccb7c3af2ee2dae03a4f0e6f78b6dbfdb
Burdy <burdy.goureige.ep@whoz.com>
2024-10-22 13:27:55 +0200

    ajout du formulaire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,3 +12,4 @@ export const routes: Routes = [
     // { path: '**', component: NotFoundComponent }
     { path: 'donation-forms', component: DonationComponent},
 ];
+
--- src/app/shared/components/personal-info-form/personal-info-form.component.ts
+++ src/app/shared/components/personal-info-form/personal-info-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ControlContainer, FormGroupDirective } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
-import {  MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { distinctUntilChanged, tap } from 'rxjs';
+
+export interface PeriodicElement {
+  selection: string;
+  don: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {selection: 'Don', don: 50},
+  {selection: 'Votre contribution à HelloAsso', don: 0}
+];
 
 
 @Component({
@@ -19,9 +31,30 @@ import {  MatTableModule } from '@angular/material/table';
     MatTableModule
   ],
   templateUrl: './personal-info-form.component.html',
-  styleUrl: './personal-info-form.component.scss'
+  styleUrl: './personal-info-form.component.scss',
+  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
+
 })
-export class PersonalInfoFormComponent {
- 
+export class PersonalInfoFormComponent implements OnInit {
+  displayedColumns: string[] = ['selection', 'don'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  total = ELEMENT_DATA.reduce((acc, val) => acc + val.don, 0);
+  formDirective =  inject(FormGroupDirective)
+
+  constructor(){
+    
+  }
+  
+  ngOnInit(): void {
+    const amount = this.formDirective.form.controls?.['donationFormGroup']?.get('amount');
+    amount?.valueChanges
+    .pipe(
+      tap(console.log),
+      distinctUntilChanged()
+    )
+    .subscribe((e:any)=> {})
+  }
+
+
 
 }
