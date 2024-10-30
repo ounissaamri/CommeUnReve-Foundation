e26e5ea9f7d39ce426244d0b7b35750fac5fb354
Ounissa Amri <78350734+ounissaamri@users.noreply.github.com>
2024-10-31 00:18:20 +0100

    ajout de la page de mention legale et politique de confidentialite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { QuiSommeNousComponent } from './pages/qui-somme-nous/qui-somme-nous.component';
 import { ArticleBlogComponent } from './pages/article-blog/article-blog.component';
 import { DonationComponent } from './pages/donation/donation.component';
+import { MentionslegalesComponent } from './pages/mentionslegales/mentionslegales.component';
+import { PolitiqueDeConfidentialiteComponent } from './pages/politique-de-confidentialite/politique-de-confidentialite.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -11,5 +13,7 @@ export const routes: Routes = [
     { path: 'articles-blog', component: ArticleBlogComponent },
     // { path: '**', component: NotFoundComponent }
     { path: 'donation-forms', component: DonationComponent},
+    { path: 'mentions-legales', component: MentionslegalesComponent},
+    { path: 'Politique-De-Confidentialite', component: PolitiqueDeConfidentialiteComponent},
 ];
 
