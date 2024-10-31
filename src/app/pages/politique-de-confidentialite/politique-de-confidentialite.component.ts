568a12fe73eec162a679b2b7f0781b9bc64c9350
Ounissa Amri <78350734+ounissaamri@users.noreply.github.com>
2024-10-31 17:06:24 +0100

    update de la page mentions legales et politique de confidentialite

--- src/app/pages/politique-de-confidentialite/politique-de-confidentialite.component.ts
+++ src/app/pages/politique-de-confidentialite/politique-de-confidentialite.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-politique-de-confidentialite',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MatCardModule,
+    MatDividerModule
   ],
   templateUrl: './politique-de-confidentialite.component.html',
   styleUrl: './politique-de-confidentialite.component.css'
