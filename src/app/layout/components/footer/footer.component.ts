0e7d18e631a43c512b4f312ab022e11aca8491c3
Ounissa Amri <78350734+ounissaamri@users.noreply.github.com>
2024-10-31 17:06:24 +0100

    update de la page mentions legales et politique de confidentialite

--- src/app/layout/components/footer/footer.component.ts
+++ src/app/layout/components/footer/footer.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
-
 @Component({
   selector: 'app-footer',
   standalone: true,
