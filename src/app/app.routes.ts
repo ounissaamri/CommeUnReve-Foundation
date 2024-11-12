c5f19e63a126e1bbadda224eb82185f02112d2af
Burdy <burdy.goureige.ep@whoz.com>
2024-11-12 13:12:13 +0100

    ajout de stripe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,11 +10,14 @@ import { PartenaireComponent } from './pages/partenaire/partenaire.component';
 import { ChoicePartenaireComponent } from './pages/choice-partenaire/choice-partenaire.component';
 import { AgirAvecNousComponent } from './pages/agir-avec-nous/agir-avec-nous.component';
 import { NosactionsComponent } from './pages/nosactions/nosactions.component';
+import { CompleteCheckoutComponent } from './pages/complete-checkout/complete-checkout.component';
+import { CancelCheckoutComponent } from './pages/cancel-checkout/cancel-checkout.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'qui-sommes-nous', component: QuiSommeNousComponent },
+    {path:'cancel', component:CancelCheckoutComponent},
     { path: 'choice-partenaire', component: ChoicePartenaireComponent},
     { path: 'partenaire', component: PartenaireComponent},
     { path: 'nos-actions', component: NosactionsComponent },
@@ -24,6 +27,7 @@ export const routes: Routes = [
     { path: 'mentions-legales', component: MentionslegalesComponent},
     { path: 'Politique-De-Confidentialite', component: PolitiqueDeConfidentialiteComponent},
     { path: 'devenir-benevole', component: BenevoleComponent },
+    {path:'complete', component:CompleteCheckoutComponent},
     // { path: '**', component: NotFoundComponent }
 ];
 
--- src/app/shared/components/donation-form/donation-form.component.ts
+++ src/app/shared/components/donation-form/donation-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild, inject} from '@angular/core';
-import { ControlContainer, FormGroupDirective, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ControlContainer, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink, RouterModule } from '@angular/router';
@@ -10,7 +10,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbar } from '@angular/material/toolbar';
 import { ToggleButtonComponent } from '../toggle-button/toggle-button.component';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
+import { toggleButtonOption } from '../../interfaces/toggle-button-options';
+import { AmountPaymentContantes, TypePaymentContantes } from "./../../constantes/toggle-button-option";
+
 @Component({
   selector: 'app-donation-form',
   standalone: true,
@@ -39,8 +42,24 @@ export class DonationFormComponent {
 
   @ViewChild('inputRef') inputRef!: ElementRef;
 
-  listAmount = [{id:1, value:20},{id:2, value:50},{id:3, value:100}]
-  listTypeDonation = [{id:1, value: 'Une fois'},{id:2, value:"Donner tous les mois"}]
+  // listAmount = [{id:1, value:20},{id:2, value:50},{id:3, value:100}]
+  // listTypeDonation = [{id:1, value: 'Une fois'},{id:2, value:"Donner tous les mois"}]
+
+  listTypeDonation : toggleButtonOption[] = [
+    {id:1,type: TypePaymentContantes.TYPE_PAYMENT.SUBSCRIPTION, libelle: TypePaymentContantes.LIBELLE.EVERY_MONTH},
+     {id:2,type: TypePaymentContantes.TYPE_PAYMENT.PAYMENT, libelle: TypePaymentContantes.LIBELLE.ONE_TIME}
+    ]
+  listAmount : toggleButtonOption[] = [
+    { id:1,libelle: AmountPaymentContantes.LIBELLE.TWENTY},
+    { id:2,libelle: AmountPaymentContantes.LIBELLE.FIFTY},
+    {id:3,libelle: AmountPaymentContantes.LIBELLE.HUNDRED}
+  ]
+
+  // faire que les toggle button retrtr pas en collision
+  // rendre pas obligatoire le champs de montant libre
+  // Faire api stripe pour le paiement
+  // faire api stripe pour abonnement
+  //
 
   elementActive: string|null = null
   elementActiveA: string|null = null
@@ -52,25 +71,50 @@ export class DonationFormComponent {
 
 
   ngOnInit(): void {
-    const amount = this.formDirective.form.controls?.['donationFormGroup']?.get('amount');
+
+    const amount = this.formDirective.form.controls?.['donationFormGroup']?.get('amount')
+    const amountOptions = this.formDirective.form.controls?.['donationFormGroup']?.get('amountOptions')
+
+    
     amount?.valueChanges
     .pipe(
       debounceTime(300),
+      filter((value)=> value !== null),
       distinctUntilChanged()
     )
-    .subscribe((amount)=> {
-      // if(amount.value !==  Number(this.inputRef.nativeElement.value)) {
-      //   this.inputRef.nativeElement.value = null
-      // }
+    .subscribe(_=> {
+        amountOptions?.setValue(null);
+        amountOptions?.clearValidators();
+        amountOptions?.updateValueAndValidity();
+        amount.addValidators(Validators.required);
+        amount.updateValueAndValidity()
+
+
+
+
     })
 
-    
 
+    amountOptions?.valueChanges
+    .pipe(
+      debounceTime(300),
+      filter((value)=> value !== null),
+      distinctUntilChanged(),
+    )
+    .subscribe(_=> {
+      amount?.setValue(null);
+      amount?.clearValidators();
+      amount?.updateValueAndValidity();
+      amountOptions.addValidators(Validators.required);
+      amountOptions.updateValueAndValidity();
+
+
+
+
+    })
   }
 
    changeColorA(element:string) {
      this.elementActiveA = element
    }
-  
- 
 }
